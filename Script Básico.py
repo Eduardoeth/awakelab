33f31e2e88f77df5ccaa85f0c691142a4c21da99
edu.t.h@outlook.es <edu.t.h@outlook.es>
2021-03-15 00:32:55 -0300

    Curso

---
+++ Script Básico.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+x = 3
+f = 3.1415926
+nombre = "Python"
+print(x)
+print(f)
+print(nombre) #no esta definida la variable nombre, esta como name
+combinacion = nombre + " " + nombre #la variable combinacion estaba mal definida en combination
+print(combinacion)
+suma = f + f
+print(suma)
+
+
+# In[ ]:
+
+
+
+
